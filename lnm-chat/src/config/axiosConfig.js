759f3b9ac7312a57bd7718c1b7101134b0fdf3d3
RitikMittal32 <ritikmittal8068@gmail.com>
2025-02-11 23:20:38 +0530

    deploy

--- lnm-chat/src/config/axiosConfig.js
+++ lnm-chat/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080"; // Local development URL
-// const BASE_URL = "https://e-mart-1.onrender.com"; // Production URL
+// const BASE_URL = "http://localhost:8080"; // Local development URL
+const BASE_URL = "https://lnmiit-bot.onrender.com"; // Production URL
 
 // Set global defaults for Axios
 axios.defaults.baseURL = BASE_URL;
