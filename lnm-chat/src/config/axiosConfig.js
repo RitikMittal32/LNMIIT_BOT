0f02b2232ab67e2bae54b86549cf7566d22756c3
RitikMittal32 <122687970+RitikMittal32@users.noreply.github.com>
2025-04-18 11:07:22 +0530

    NgrokDomain

--- lnm-chat/src/config/axiosConfig.js
+++ lnm-chat/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:8080"; // Local development URL
-const BASE_URL = "https://84f9-115-243-103-7.ngrok-free.app"; // Production URL
+const BASE_URL = "https://vocal-brave-stinkbug.ngrok-free.app"; // Production URL
 
 // Set global defaults for Axios
 axios.defaults.baseURL = BASE_URL;
