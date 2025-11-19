e87b738e1f71ae5ae0f223909faf9a38f3ea87e3
RitikMittal32 <122687970+RitikMittal32@users.noreply.github.com>
2025-11-19 17:10:27 +0530

    Update axiosConfig.js

--- lnm-chat/src/config/axiosConfig.js
+++ lnm-chat/src/config/axiosConfig.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:8080"; // Local development URL
-const BASE_URL = "https://0d9acd4909dd.ngrok-free.app"; // Production URL
+const BASE_URL = "https://b327062f705e.ngrok-free.app"; // Production URL
 
 // Set global defaults for Axios
 axios.defaults.baseURL = BASE_URL;
 
 export default axios;
+
 
